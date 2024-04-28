df1013bb3aa9ed1459ec1e75e4924e9ac12590de
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-28 16:06:29 +0530

    RegistrationPage: This is a functional component that represents the registration page of the application. It contains a form for users to input their desired username and password to create an account.

--- frontend/src/RegistrationPage.jsx
+++ frontend/src/RegistrationPage.jsx
@@ -2,3 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const RegistrationPage = () => {
+    
+};
+
+export default RegistrationPage;
