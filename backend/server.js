ff024ece0edb84c05d2d5785462fc3a3bfe87f0b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-28 15:50:04 +0530

    app.post('/register', ...) and app.post('/login', ...): These lines define the routes for handling POST requests to the '/register' and '/login' endpoints, respectively. When a POST request is sent to these endpoints, the corresponding callback functions are executed to handle the request. Registration Route: This route handles user registration. It expects a JSON object containing the username and password fields in the request body. It creates a new User instance using the User model and saves it to the database. If successful, it responds with a status code of 201 (Created) and a JSON object containing a success message. If an error occurs, it responds with a status code of 500 (Internal Server Error) and an error message. Login Route: This route handles user login. It expects a JSON object containing the username and password fields in the request body. It queries the database for a user with the provided username. If a user is found, it checks if the provided password matches the stored password. If the credentials are valid, it responds with a status code of 200 (OK) and a success message. If the credentials are invalid or an error occurs, it responds with a status code of 401 (Unauthorized) and an error message.

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,38 @@ app.use(express.json());
 //Enable CORS
 app.use(cors())
 
+//Registration
+app.post('/register',async(req,res) => {
+    try{
+        const {username,password} = req.body;
+        console.log(req.body)
+        const user = new User({username,password});
+        await user.save();
+        res.status(201).json({message:'Registration Successful'});
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({message:'Registration failed'});
+    }
+})
+
+//Login
+app.post('/login',async(req,res)=>{
+    try{
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+
+        if(!user){
+            return res.status(401).json({message:'Invalid username or Password'});
+        }
+
+        if(user.password !== password){
+            return res.status(401).json({message:'Invalid username or password'});
+        }
+        res.status(200).json({message:'Login successful'})
+    }
+    catch(error){
+        res.status(500).json({message:'Login failed'})
+    }
+})
+
