3efb4545f9abb7fc310ad55b3e246279e1a96440
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-28 15:58:44 +0530

    App: This functional component is the root component of the application. It renders the BrowserRouter component, which provides the routing functionality for the entire application. Inside the BrowserRouter component, there is a Routes component, which contains multiple Route components. Each Route component specifies a URL path and the corresponding component to render when that path is matched. The 'element' prop of each Route component specifies the component to render when the URL matches the specified path.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,4 +3,15 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import LoginPage from './LoginPage'
 import RegistrationPage from './RegistrationPage';
 
+const App = () => {
+  return (
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/registration' element={<RegistrationPage/>}/>
+      </Routes>
+      </BrowserRouter>
+  )
+}
+
 
