ab3a808cf32a598018aa610f2391ca792a0957c1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-21 13:06:51 +0530

    Changed the server.js: Moved node_modules,.env.package.json,package-lock.json into the root directory:
    Updated the scripts to host frontend from backend:

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async() => {
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/myDatabase');
+        await mongoose.connect('mongodb+srv://nishkarshgupta671:nishkarshgupta671@mydatabase.flyze.mongodb.net/?retryWrites=true&w=majority&appName=myDatabase');
         console.log('Connected to MongoDB');
     }
     catch(error){
--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
-const connectDB = require('./db/dbConnection');
-const User = require('./db/user');
-const cors = require('cors');
+const connectDB = require("./db/dbConnection");
+const User = require("./db/user");
+const cors = require("cors");
+const path = require("path");
+
+const __dirName = path.resolve();
+console.log(__dirName);
 
 //Middleware for parsing JSON
 app.use(express.json());
 
 //Enable CORS
-app.use(cors())
+app.use(cors());
 
 //Registration
-app.post('/register',async(req,res) => {
-    try{
-        const {username,password} = req.body;
-        console.log(req.body)
-        const user = new User({username,password});
-        await user.save();
-        res.status(201).json({message:'Registration Successful'});
-    }
-    catch(error){
-        console.log(error)
-        res.status(500).json({message:'Registration failed'});
-    }
-})
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body);
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: "Registration Successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
 
 //Login
-app.post('/login',async(req,res)=>{
-    try{
-        const {username,password} = req.body;
-        const user = await User.findOne({username});
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
 
-        if(!user){
-            return res.status(401).json({message:'Invalid username or Password'});
-        }
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or Password" });
+    }
 
-        if(user.password !== password){
-            return res.status(401).json({message:'Invalid username or password'});
-        }
-        res.status(200).json({message:'Login successful'})
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalid username or password" });
     }
-    catch(error){
-        res.status(500).json({message:'Login failed'})
-    }
-})
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed" });
+  }
+});
 
 connectDB();
 
-app.listen(port,()=> {
- console.log('Server is listening on Post 8000')
+app.use(express.static(path.join(__dirName, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirName, "frontend", "dist", "index.html"));
+})
+
+app.listen(port, () => {
+  console.log("Server is listening on Post 8000");
 });
